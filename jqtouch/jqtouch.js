f7b6dc6fa4831c192c30d1028386c3c2f9b2a8c1
jonathanstark <jstark@jonathanstark.com>
2010-10-08 10:14:02 -0400

    Added experimental support for icon4 for retina display (thanks to bjoernwibben)

--- jqtouch/jqtouch.js
+++ jqtouch/jqtouch.js
@@ -68,6 +68,7 @@
                 fullScreen: true,
                 fullScreenClass: 'fullscreen',
                 icon: null,
+                icon4: null, // experimental
                 touchSelector: 'a, .touch',
                 popSelector: '.pop',
                 preloadImages: false,
@@ -88,11 +89,20 @@
                     (new Image()).src = jQTSettings.preloadImages[i];
                 };
             }
-            // Set icon
-            if (jQTSettings.icon) {
-                var precomposed = (jQTSettings.addGlossToIcon) ? '' : '-precomposed';
-                hairExtensions += '<link rel="apple-touch-icon' + precomposed + '" href="' + jQTSettings.icon + '" />';
+            // Set appropriate icon (retina display stuff is experimental)
+            if (jQTSettings.icon || jQTSettings.icon4) {
+                var precomposed, appropriateIcon;
+                if (jQTSettings.icon4 && window.devicePixelRatio && window.devicePixelRatio === 2) {
+                    appropriateIcon = jQTSettings.icon4;
+                } else if (jQTSettings.icon) {
+                    appropriateIcon = jQTSettings.icon;
+                }
+                if (appropriateIcon) {
+                    precomposed = (jQTSettings.addGlossToIcon) ? '' : '-precomposed';
+                    hairExtensions += '<link rel="apple-touch-icon' + precomposed + '" href="' + appropriateIcon + '" />';
+                }
             }
+
             // Set startup screen
             if (jQTSettings.startupScreen) {
                 hairExtensions += '<link rel="apple-touch-startup-image" href="' + jQTSettings.startupScreen + '" />';
