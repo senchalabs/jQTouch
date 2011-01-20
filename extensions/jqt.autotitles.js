10a599651b5a6bdbc6232ff6fa71d8f48e88b5c7
Daniel J. Pinter <DataZombies@gmail.com>
2011-01-20 18:00:50 -0500

    Fixed autotitle demo's title & existing title in target page logic.
    Signed-off-by: Daniel J. Pinter <DataZombies@gmail.com>

--- extensions/jqt.autotitles.js
+++ extensions/jqt.autotitles.js
@@ -1,51 +1,48 @@
 /*
 
-            _/    _/_/    _/_/_/_/_/                              _/       
-               _/    _/      _/      _/_/    _/    _/    _/_/_/  _/_/_/    
-          _/  _/  _/_/      _/    _/    _/  _/    _/  _/        _/    _/   
-         _/  _/    _/      _/    _/    _/  _/    _/  _/        _/    _/    
-        _/    _/_/  _/    _/      _/_/      _/_/_/    _/_/_/  _/    _/     
-       _/                                                                  
+            _/    _/_/    _/_/_/_/_/                              _/
+               _/    _/      _/      _/_/    _/    _/    _/_/_/  _/_/_/
+          _/  _/  _/_/      _/    _/    _/  _/    _/  _/        _/    _/
+         _/  _/    _/      _/    _/    _/  _/    _/  _/        _/    _/
+        _/    _/_/  _/    _/      _/_/      _/_/_/    _/_/_/  _/    _/
+       _/
     _/
 
     Created by David Kaneda <http://www.davidkaneda.com>
     Documentation and issue tracking on Google Code <http://code.google.com/p/jqtouch/>
-    
+
     Special thanks to Jonathan Stark <http://jonathanstark.com/>
     and pinch/zoom <http://www.pinchzoom.com/>
-    
+
     (c) 2009 by jQTouch project members.
     See LICENSE.txt for license.
 
 */
 
-(function($) {
-    if ($.jQTouch)
-    {
-        $.jQTouch.addExtension(function AutoTitles(jQT){
-            
-            var titleSelector='.toolbar h1';
-
-            $(function(){
-                $('#jqt').bind('pageAnimationStart', function(e, data){
-                    if (data.direction === 'in'){
-                        var $title = $(titleSelector, $(e.target));
-                        var $ref = $(e.target).data('referrer');
-                        if ($title.length && $ref && $title.html() === ''){
+(function ($) {
+    if ($.jQTouch) {
+        $.jQTouch.addExtension(function AutoTitles(jQT) {
+            var titleSelector = '.toolbar h1';
+            $(function () {
+                $('#jqt').bind('pageAnimationStart', function (e, data) {
+                    if (data.direction === 'in') {
+                        var $ref = $(e.target).data('referrer'),
+                            $title = $(titleSelector, $(e.target));
+                        if ($title.length && $ref && $title.data('title') !== $ref.text()) {
                             $title.html($ref.text());
+                            $title.data('title', $ref.text());
                         }
                     }
                 });
             });
-            
-            function setTitleSelector(ts){
-                titleSelector=ts;
-            }
-            
-            return {
-                setTitleSelector: setTitleSelector
+
+            function setTitleSelector(ts) {
+                titleSelector = ts;
             }
 
+            return {
+                setTitleSelector: setTitleSelector
+            };
         });
     }
 })(jQuery);
