ac66a8172cf49264e202fedad70807bc345df3a4
Daniel J. Pinter <DataZombies@gmail.com>
2011-02-26 12:48:14 -0500

    jqt.autottitles.js: set page title and back button from referring page; honors hardcoded title and back button text; new demo.

--- extensions/jqt.autotitles.js
+++ extensions/jqt.autotitles.js
@@ -17,36 +17,90 @@
     (c) 2009 by jQTouch project members.
     See LICENSE.txt for license.
 
+=======
+Modified to honor hardcoded titles; set back button text - Daniel J. Pinter - DataZombies
+
 */
 
 (function ($) {
     if ($.jQTouch) {
         $.jQTouch.addExtension(function AutoTitles(jQT) {
-            var titleSelector = '.toolbar h1';
+            var backButtonSelector = '.toolbar .back',
+                parentSelector = '#jqt > div, #jqt > form',
+                titleSelector = '.toolbar h1';
 
             $(function () {
+                var eventIn, eventOut;
+
+                eventIn = function (e) {
+                    var $backButton = $(e.target) !== null ? $(backButtonSelector, $(e.target)) : null,
+                        $referrer = $(e.target).data('referrer') !== null ? $(e.target).data('referrer') : null,
+                        $title = $(e.target) !== null ? $(titleSelector, $(e.target)) : null;
+                    
+                    if ($backButton.data('hardCoded') === null) {
+                        $backButton.data('hardCoded', $backButton.text() !== '');
+                    }
+                    if ($backButton && $referrer && !$backButton.data('hardCoded')) {
+                        $backButton.data('backButton', $backButton.text());
+                        $backButton.text($referrer.parents(parentSelector).find(titleSelector).text());
+                    }
+                    
+                    if ($title.data('hardCoded') === null) {
+                        $title.data('hardCoded', $title.text() !== '');
+                    }
+                    if ($title && $referrer && !$title.data('hardCoded')) {
+                        $title.data('title', $title.text());
+                        $title.text($referrer.text());
+                    }
+                };
+
+                eventOut = function (e) {
+                    var $backButton = $(e.target) !== null ? $(backButtonSelector, $(e.target)) : null,
+                        $title = $(e.target) !== null ? $(titleSelector, $(e.target)) : null;
+                    
+                    if ($backButton) {
+                        $backButton.text($backButton.data('backButton'));
+                        $backButton.data('backButton', null);
+                    }
+                    
+                    if ($title) {
+                        $title.text($title.data('title'));
+                        $title.data('title', null);
+                    }
+                };
+
                 $('#jqt').bind('pageAnimationStart', function (e, data) {
-                    var $ref = $(e.target).data('referrer'),
-                        $title = $(titleSelector, $(e.target));
-                    if (data.direction === 'in') {
-                        if ($title.html() === '' && $ref && $title.data('title') !== $ref.text()) {
-                            $title.data('title', $title.html());
-                            $title.html($ref.text());
-                        }
+                    if (data.direction === 'out') {
+                        eventOut(e);
                     } else {
-                        if (typeof($title.data('title')) !== 'undefined' && $title.data('title') !== null) {
-                            $title.html($title.data('title'));
-                            $title.data('title', null);
-                        }
+                        eventIn(e);
+                    }
+                });
+
+                $('#jqt').bind('pageAnimationEnd', function (e, data) {
+                    if (data.direction === 'in') {
+                        eventIn(e);
+                    } else {
+                        eventOut(e);
                     }
                 });
             });
+
+            function setBackButtonSelector(bs) {
+                backButtonSelector = bs;
+            }
+
+            function setParentSelector(ps) {
+                parentSelector = ps;
+            }
 
             function setTitleSelector(ts) {
                 titleSelector = ts;
             }
 
             return {
+                setBackButtonSelector: setBackButtonSelector,
+                setParentSelector: setParentSelector,
                 setTitleSelector: setTitleSelector
             };
         });
