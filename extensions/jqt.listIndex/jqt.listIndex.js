f63f0595ca155dfdddfdf1b2982f1f49d0de75fe
Daniel J. Pinter <DataZombies@gmail.com>
2011-03-03 17:47:33 -0500

    Beginning listIndex extension

---
+++ extensions/jqt.listIndex/jqt.listIndex.js
@@ -0,0 +1,38 @@
+/*
+
+            _/    _/_/    _/_/_/_/_/                              _/       
+               _/    _/      _/      _/_/    _/    _/    _/_/_/  _/_/_/    
+          _/  _/  _/_/      _/    _/    _/  _/    _/  _/        _/    _/   
+         _/  _/    _/      _/    _/    _/  _/    _/  _/        _/    _/    
+        _/    _/_/  _/    _/      _/_/      _/_/_/    _/_/_/  _/    _/     
+       _/                                                                  
+    _/
+
+    Created by David Kaneda <http://www.davidkaneda.com>
+    Documentation and issue tracking on Google Code <http://code.google.com/p/jqtouch/>
+    
+    Special thanks to Jonathan Stark <http://jonathanstark.com/>
+    and pinch/zoom <http://www.pinchzoom.com/>
+    
+    (c) 2009 by jQTouch project members.
+    See LICENSE.txt for license.
+
+=======
+jqt.listIndex.js - Daniel J. Pinter - DataZombies
+
+Implementation of the table index
+
+Change Log
+--------------------------------------------------------------------------------
+
+*/
+
+(function($) {
+    if ($.jQTouch)
+    {
+        $.jQTouch.addExtension(function listIndex(jQT){
+
+
+        });
+    }
+})(jQuery);
