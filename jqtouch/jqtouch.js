8c050bae5f9c1992c9dd29df7837b9c91b60e1cc
Mark Schmale <ma.schmale@googlemail.com>
2011-05-20 04:17:24 -0700

    put a var in front of delta, too

--- jqtouch/jqtouch.js
+++ jqtouch/jqtouch.js
@@ -89,7 +89,7 @@
 
         function _debug(message) {
             var now = (new Date).getTime();
-            delta = now - lastTime;
+            var delta = now - lastTime;
             lastTime = now;
             if (jQTSettings.debug) {
                 if (message) {
