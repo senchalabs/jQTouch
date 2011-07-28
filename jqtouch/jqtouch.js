0293bf2f6e47edba237ab65e84beb6b0c119cac5
Antti Tapaninen <antti@applifier.com>
2011-07-28 13:52:48 +0300

    fix jQuery 1.6+ compatibility (patch by szarouski)

--- jqtouch/jqtouch.js
+++ jqtouch/jqtouch.js
@@ -596,7 +596,7 @@
 
             // Init some vars
             var target = $el.attr('target'),
-                hash = $el.attr('hash'),
+                hash = $el.attr('hash') || $el.prop('hash'), // jQuery 1.6+ attr vs. prop
                 animation = null;
 
             if ($el.isExternalLink()) {
