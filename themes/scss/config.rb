6bd5f7874db665adb69e1a8148d734ef0b86b0a4
David Kaneda <dk@morfunk.com>
2011-11-21 18:11:17 -0800

    Style updates
    * Turning off non-WebKit CSS (for now)
    * Lots of new color-independence in jqtouch.scss.
    * Plain toolbar buttons are now CSS
    * All list items now use the same background gradient, and arrow/forward classes just use an after element to position the icon on top.
    * Fixing form styling
    * Update accessories
    * Switch built SCSS versions to "expanded" — will avoid merge conflicts
    * Nixing redundant rules

--- themes/scss/config.rb
+++ themes/scss/config.rb
@@ -3,5 +3,5 @@ require 'accessories'
 sass_path = File.dirname(__FILE__)
 css_path = File.join(sass_path, "..", "css")
 
-output_style = :compressed
+output_style = :expanded
 environment = :production
