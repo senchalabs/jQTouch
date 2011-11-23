230c7d71180337c988e76b1c67042260247c6a54
Thomas @ BeeDesk <thomas@beedesk.com>
2011-11-23 03:40:45 -0800

    Implemented theme css switcher as an extension. Updated demos to include it. To make an css 'theme-switchable', use <link type='stylesheet'/> instead of <style>@Import</>, then add title attribute to the <link>. If no <link title='name'> specified, the extension will add stylesheet for you based on the default list. The defaults list, 'themeIncluded', can be passed in as regular jQT options. jQT developer should keep the default defined in the extensionup-to-date.

--- jqtouch/jqtouch.js
+++ jqtouch/jqtouch.js
@@ -42,6 +42,7 @@
                 addGlossToIcon: true,
                 backSelector: '.back, .cancel, .goback',
                 cacheGetRequests: true,
+                scriptpath: jQTouchCore.prototype.scriptpath,
                 debug: false,
                 fallback2dAnimation: 'fade',
                 defaultAnimation: 'slideleft',
@@ -752,7 +753,7 @@
         return publicObj;
     };
     
-    jQTouchCore.prototype.corepath = $("script").last().attr("src").split('?')[0].split('/').slice(0, -1).join('/')+'/'; 
+    jQTouchCore.prototype.scriptpath = $("script").last().attr("src").split('?')[0].split('/').slice(0, -1).join('/')+'/'; 
     jQTouchCore.prototype.extensions = [];
 
     // If Zepto exists, jQTouch will use Zepto. Otherwise, a bridge should initialize
