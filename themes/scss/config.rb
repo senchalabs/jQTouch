76c0dc71ebfd16744d44ce2eb193c7fe029ae04e
David Kaneda <dk@morfunk.com>
2011-12-17 12:31:43 -0800

    Updating SCSS to use new Recipes mixing.

--- themes/scss/config.rb
+++ themes/scss/config.rb
@@ -1,4 +1,5 @@
-require File.join(File.dirname(__FILE__), '..', 'accessories', 'lib', 'accessories')
+# Manually bring in Compass Recipes for styling shortcuts
+require File.join(File.dirname(__FILE__), '..', 'compass-recipes', 'lib', 'compass-recipes')
 
 sass_path = File.dirname(__FILE__)
 css_path = File.join(sass_path, "..", "css")
