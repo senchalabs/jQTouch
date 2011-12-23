5e77dcdc20855cd00bff3c69588d7fa384fe7ee0
David Kaneda <dk@morfunk.com>
2011-12-22 23:44:45 -0800

    Provide map fallback on GeoLocation error, improvements to plugin.

--- extensions/jqt.location.js
+++ extensions/jqt.location.js
@@ -1,45 +1,27 @@
-/*
-
-            _/    _/_/    _/_/_/_/_/                              _/       
-               _/    _/      _/      _/_/    _/    _/    _/_/_/  _/_/_/    
-          _/  _/  _/_/      _/    _/    _/  _/    _/  _/        _/    _/   
-         _/  _/    _/      _/    _/    _/  _/    _/  _/        _/    _/    
-        _/    _/_/  _/    _/      _/_/      _/_/_/    _/_/_/  _/    _/     
-       _/                                                                  
-    _/
-
-    Created by David Kaneda <http://www.davidkaneda.com>
-    Documentation and issue tracking on Google Code <http://code.google.com/p/jqtouch/>
-    
-    Special thanks to Jonathan Stark <http://jonathanstark.com/>
-    and pinch/zoom <http://www.pinchzoom.com/>
-    
-    (c) 2009 by jQTouch project members.
-    See LICENSE.txt for license.
-
-*/
-
 (function($) {
     if ($.jQTouch)
     {
         $.jQTouch.addExtension(function Location(){
             
-            var latitude, longitude, callback;
+            var latitude, longitude, callback, callback2;
             
-            function checkGeoLocation() {
-                return navigator.geolocation;
-            }
-            function updateLocation(fn) {
-                if (checkGeoLocation())
+            function updateLocation(fn, fn2) {
+                if (navigator.geolocation)
                 {
                     callback = fn;
-                    navigator.geolocation.getCurrentPosition(savePosition);
+                    callback2 = fn2;
+                    navigator.geolocation.getCurrentPosition(savePosition, failResponse);
                     return true;
                 } else {
                     console.log('Device not capable of geo-location.');
                     fn(false);
                     return false;
-                }                
+                }
+            }
+            function failResponse(error){
+                if (callback2) {
+                    callback2(error);
+                }
             }
             function savePosition(position) {
                 latitude = position.coords.latitude;
@@ -53,7 +35,8 @@
                     return {
                         latitude: latitude,
                         longitude: longitude
-                    }
+                    };
+                    
                 } else {
                     console.log('No location available. Try calling updateLocation() first.');
                     return false;
@@ -62,7 +45,7 @@
             return {
                 updateLocation: updateLocation,
                 getLocation: getLocation
-            }
+            };
         });
     }
-})(jQuery);
+})($);
